cebb90a0bbe5d7a0e55da7c32310acbd9e259599
Diordany van Hemert <diordany.van.hemert@gmail.com>
2025-05-24 00:30:00 +0200

    Reorder settings

--- settings.lua
+++ settings.lua
@@ -2,6 +2,7 @@ data:extend({
   {
     type = "double-setting",
     name = "speed-settings-force-lab",
+    order = "cb",
     setting_type = "runtime-global",
     default_value = 0.0,
     minimum_value = -1.0
@@ -9,6 +10,7 @@ data:extend({
   {
     type = "double-setting",
     name = "speed-settings-force-worker-robot",
+    order = "ca",
     setting_type = "runtime-global",
     default_value = 0.0,
     minimum_value = -1.0
@@ -16,6 +18,7 @@ data:extend({
   {
     type = "double-setting",
     name = "speed-settings-game",
+    order = "aa",
     setting_type = "runtime-global",
     default_value = 1.0,
     minimum_value = 0.1
@@ -23,6 +26,7 @@ data:extend({
   {
     type = "double-setting",
     name = "speed-settings-player-crafting",
+    order = "bc",
     setting_type = "runtime-global",
     default_value = 0.0,
     minimum_value = -1.0
@@ -30,6 +34,7 @@ data:extend({
   {
     type = "double-setting",
     name = "speed-settings-player-mining",
+    order = "bb",
     setting_type = "runtime-global",
     default_value = 0.0,
     minimum_value = -1.0
@@ -37,6 +42,7 @@ data:extend({
   {
     type = "double-setting",
     name = "speed-settings-player-running",
+    order = "ba",
     setting_type = "runtime-global",
     default_value = 0.0,
     minimum_value = -1.0
